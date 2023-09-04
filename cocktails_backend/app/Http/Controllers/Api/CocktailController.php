f019ec9067049589f73b355140befe063d925485
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-09-04 16:04:19 +0200

    created model for cocktail resource

--- cocktails_backend/app/Http/Controllers/Api/CocktailController.php
+++ cocktails_backend/app/Http/Controllers/Api/CocktailController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cocktail;
 
 class CocktailController extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function show(String $id){
+
+    }
 }
