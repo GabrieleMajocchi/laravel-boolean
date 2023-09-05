5cfd3a52c810f741d05965ab5cf6b0e47af57d14
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-05 16:55:07 +0200

    created cocktail and ingredient pivot table seeder

---
+++ cocktails_backend/database/seeders/CocktailIngredientSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cocktail;
+use App\Models\Ingredient;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CocktailIngredientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+    }
+}
