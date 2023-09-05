9bdb404404ac5d65aa45a36e84a2d2f6dd9f6a5b
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-09-05 15:59:28 +0200

    fixed string type in type column ingredients table

--- cocktails_backend/database/migrations/2023_09_05_132321_create_ingredients_table.php
+++ cocktails_backend/database/migrations/2023_09_05_132321_create_ingredients_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->text('type');
+            $table->string('type');
             $table->timestamps();
         });
     }
