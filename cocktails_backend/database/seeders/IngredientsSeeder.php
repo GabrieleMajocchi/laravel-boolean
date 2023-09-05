22237a46b194bf06d25c22648023935e01e0d787
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-05 17:40:29 +0200

    cleared ingredients seeder

--- cocktails_backend/database/seeders/IngredientsSeeder.php
+++ cocktails_backend/database/seeders/IngredientsSeeder.php
@@ -15,198 +15,23 @@ class IngredientsSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 1; $i <= 50; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
+        for ($ii = 1; $ii <= 12; $ii++) {
+            $end = 50 * $ii;
+            $start = $end - 49;
+            for ($i = $start; $i <= $end; $i++) {
+                $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
+                $data = $response->json();
 
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
+                if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
+                    $ingredient = $data['ingredients'][0];
+                    $newIngredient = new Ingredient();
+                    $newIngredient->name = $ingredient['strIngredient'];
+                    $newIngredient->description = $ingredient['strDescription'];
+                    $newIngredient->type = $ingredient['strType'];
+                    $newIngredient->save();
+                }
             }
-        }
         sleep(30);
-        for ($i = 51; $i <= 100; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 101; $i <= 150; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 151; $i <= 200; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 201; $i <= 250; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 251; $i <= 300; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 301; $i <= 350; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 351; $i <= 400; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 401; $i <= 450; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 451; $i <= 500; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 501; $i <= 550; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 551; $i <= 600; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
-        }
-        sleep(30);
-        for ($i = 601; $i <= 650; $i++) {
-            $response = Http::withOptions(['verify' => false])->get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=' . $i);
-            $data = $response->json();
-
-            if (isset($data['ingredients']) && !is_null($data['ingredients'])) {
-                $ingredient = $data['ingredients'][0];
-                $newIngredient = new Ingredient();
-                $newIngredient->name = $ingredient['strIngredient'];
-                $newIngredient->description = $ingredient['strDescription'];
-                $newIngredient->type = $ingredient['strType'];
-                $newIngredient->save();
-                sleep(0.2);
-            }
         }
     }
 }
