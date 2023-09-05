69eb01818168e2410156ddd782c0654b40efaaae
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-05 17:54:40 +0200

    added pivot table seeder call on database seeder

--- cocktails_backend/database/seeders/DatabaseSeeder.php
+++ cocktails_backend/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CocktailsSeeder::class,
             IngredientsSeeder::class,
+            CocktailIngredientSeeder::class,
         ]);
     }
 }
