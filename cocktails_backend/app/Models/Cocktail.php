d647fb80dbda3d3da7573647266315ce5637e210
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-09-05 15:35:22 +0200

    added relationship many to many between ingredients and cocktails

--- cocktails_backend/app/Models/Cocktail.php
+++ cocktails_backend/app/Models/Cocktail.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cocktail extends Model
 {
     use HasFactory;
+
+    public function ingredients(){
+        return $this->belongsToMany(Ingredient::class);
+    }
 }
--- cocktails_backend/app/Models/Ingredient.php
+++ cocktails_backend/app/Models/Ingredient.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Ingredient extends Model
 {
     use HasFactory;
+
+    public function cocktails(){
+        return $this->belongsToMany(Cocktail::class);
+    }
 }
